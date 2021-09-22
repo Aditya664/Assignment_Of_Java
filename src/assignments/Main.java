d46e3d473836d74efe18a4c43fa2d43215bf2139
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 10:50:37 -0700

    Added files in .gitignore

--- src/assignments/Main.java
+++
@@ -1,8 +0,0 @@
-package assignments;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
--- src/assignments/_02_FirstJava/Q1.java
+++ src/assignments/_02_FirstJava/Q1.java
@@ -1,4 +1,4 @@
-package assignments._2.FirstJava;
+package assignments._02_FirstJava;
 
 import java.util.Scanner;
 
