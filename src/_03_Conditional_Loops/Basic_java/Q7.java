d719db9f237c3bc1b19c98a13f67204ea5d2cc63
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 21:22:54 -0700

    Added Que no. 7

--- src/_03_Conditional_Loops/Basic_java/Q7.java
+++ src/_03_Conditional_Loops/Basic_java/Q7.java
@@ -1,4 +1,14 @@
 package _03_Conditional_Loops.Basic_java;
 
+import java.util.Scanner;
+
 public class Q7 {
+    //W.A.P to calculate the area of Equilateral triangle
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter sides of triangle: ");
+        double sides = in.nextDouble();
+        double area = (Math.sqrt(3)/4)*sides*sides;
+        System.out.println("Area of Equilateral triangle is: " +area);
+    }
 }
