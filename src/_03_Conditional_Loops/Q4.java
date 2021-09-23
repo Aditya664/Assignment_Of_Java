81cb47537da730841fc8080f6c03cd47255459d1
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 20:27:51 -0700

    Added Que no. 4

---
+++ src/_03_Conditional_Loops/Q4.java
@@ -0,0 +1,17 @@
+package _03_Conditional_Loops;
+
+import java.util.Scanner;
+
+public class Q4 {
+    //W.A.P to calculate the area of isosceles triangle.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter base of triangle: ");
+        float base = in.nextFloat();
+        System.out.println("Please enter height of triangle: ");
+        float height = in.nextFloat();
+        float area = base * height/2;
+        System.out.println("Area of isosceles triangle is: " +area);
+
+    }
+}
