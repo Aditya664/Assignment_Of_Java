491375132bffe7589d0356e042a66b585340447f
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 20:33:20 -0700

    Added Que no. 5

---
+++ src/_03_Conditional_Loops/Q5.java
@@ -0,0 +1,16 @@
+package _03_Conditional_Loops;
+
+import java.util.Scanner;
+
+public class Q5 {
+    //W.A.P to calculate the area of parallelogram.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter base: ");
+        int base = in.nextInt();
+        System.out.println("Please enter height: ");
+        int height = in.nextInt();
+        int area = base*height;
+        System.out.println("Area of parallelogram is: " +area);
+    }
+}
