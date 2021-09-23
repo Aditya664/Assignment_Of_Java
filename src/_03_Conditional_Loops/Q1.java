762114511b546799b6b2a16cc61070306dc9a33e
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 20:05:17 -0700

    Added Que no. 1

--- src/_03_Conditional_Loops/Q1.java
+++ src/_03_Conditional_Loops/Q1.java
@@ -1,4 +1,14 @@
 package _03_Conditional_Loops;
 
+import java.util.Scanner;
+
 public class Q1 {
+    //W.A.P to calculate the area of circle.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter radius of cricle: ");
+        int radius = in.nextInt();
+        double area = Math.PI*radius*radius;
+        System.out.println("Area of circle is: " +area);
+    }
 }
