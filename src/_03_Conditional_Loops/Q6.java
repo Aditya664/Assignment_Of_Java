4d9764d140684a74c282f9a3770cb0eb8ecd4708
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 20:39:29 -0700

    Added Que no. 6

---
+++ src/_03_Conditional_Loops/Q6.java
@@ -0,0 +1,16 @@
+package _03_Conditional_Loops;
+
+import java.util.Scanner;
+
+public class Q6 {
+    //W.A.P to calculate the area of rhombus.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter diagonal 1 : ");
+        double d1 = in.nextDouble();
+        System.out.println("Please enter diagonal 2 : ");
+        double d2 = in.nextDouble();
+        double area = (d1*d2)/2;
+        System.out.println("Area of rhombus is: "+area );
+    }
+}
