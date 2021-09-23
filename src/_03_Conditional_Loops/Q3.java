9ee06d01040eef44a3e85a637a1c341bbb1e2bc4
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 20:23:08 -0700

    Added Que no. 3

--- src/_03_Conditional_Loops/Q3.java
+++ src/_03_Conditional_Loops/Q3.java
@@ -1,6 +1,18 @@
 package _03_Conditional_Loops;
 
+import java.util.Scanner;
+
 public class Q3 {
     //W.A.P to calculate the area of rectangle
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter Length of the triangle: ");
+        float length = in.nextFloat();
+        System.out.println("Please enter Weight of the triangle: ");
+        float weight = in.nextFloat();
+        float area = weight * length;
+        System.out.println("Area of the triangle: " +area);
+
+    }
 
 }
