ca8d3174d1b53e9583a591d59f4ea2de3ed0e63f
Aditya <Adityadeshmukh7350@gmail.com>
2021-09-29 04:52:13 -0700

    Added Que no 1

---
+++ src/_03_Conditional_Loops/Intermediate_Java/Q1/DoWhile.java
@@ -0,0 +1,19 @@
+package _03_Conditional_Loops.Intermediate_Java.Q1;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int num;
+        int fact = 1;
+        int i = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Enter number to find factorial: ");
+        num = in.nextInt();
+        do{
+            fact = fact * i;
+            i++;
+        }while(num >=i );
+        System.out.println("Factorial of " + num + " is:" + fact);
+    }
+}
---
+++ src/_03_Conditional_Loops/Intermediate_Java/Q1/Q1_While.java
@@ -0,0 +1,22 @@
+package _03_Conditional_Loops.Intermediate_Java.Q1;
+
+import java.util.Scanner;
+
+public class Q1_While {
+    public static void main(String[] args) {
+        //Factorial Program In Java
+        int num;
+        int fact = 1;
+        int i = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter number to find factorial: ");
+        num = in.nextInt();
+
+        while(i <= num){
+            fact = fact * i;
+            i++;
+
+        }
+        System.out.println("Factorial of " + num + "is: " + fact);
+    }
+}
